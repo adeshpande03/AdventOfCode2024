f38ca3bdc0be98cd9c4194dc5b3d4aa2f9519941
Akhil Deshpande <deshpande.akhil.s@gmail.com>
2024-12-01 00:39:51 -0600

    day 1 optimized

--- Day01/day01.py
+++ Day01/day01.py
@@ -1,9 +1,10 @@
 from pathlib import Path
-
+from collections import Counter
 
 def part1(filename="input.txt"):
     with Path(__file__).with_name(filename).open("r") as f:
         lines = f.read().splitlines()
+    t = 0
     l1 = []
     l2 = []
     for line in lines:
@@ -12,7 +13,6 @@ def part1(filename="input.txt"):
         l2.append(n2)
     l1.sort()
     l2.sort()
-    t = 0
     for i in range(len(l1)):
         t += abs(l1[i] - l2[i])
     return t
@@ -22,17 +22,16 @@ def part1(filename="input.txt"):
 def part2(filename="input.txt"):
     with Path(__file__).with_name(filename).open("r") as f:
         lines = f.read().splitlines()
+    t = 0
     l1 = []
     l2 = []
     for line in lines:
         n1, n2 = int(line.split()[0]), int(line.split()[-1])
         l1.append(n1)
         l2.append(n2)
-    l1.sort()
-    l2.sort()
-    t = 0
-    for i in range(len(l1)):
-        t += l1[i] * l2.count(l1[i])
+    l2 = Counter(l2)
+    for i in l1:
+        t += i * l2[i]
     return t
 
 if __name__ == "__main__":
