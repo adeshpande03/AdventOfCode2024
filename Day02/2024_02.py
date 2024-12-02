bb714944c2d1978c5e4a7d8f20937611cfd06c0f
Akhil Deshpande <deshpande.akhil.s@gmail.com>
2024-12-01 23:30:15 -0600

    day 2 :( bad ranks

--- Day02/2024_02.py
+++ Day02/2024_02.py
@@ -6,13 +6,40 @@ def part1(filename="input.txt"):
     with Path(__file__).with_name(filename).open("r") as f:
         lines = f.read().splitlines()
     ans = 0
+    
+    for l in lines:
+        l = list(map(int, l.split()))
+        if l == list(sorted(l)):
+            if all(1 <= l[i] - l[i - 1] <= 3 for i in range(1, len(l))):
+                ans += 1
+        elif l == list(sorted(l, reverse=True)):
+            if all(-1 >= l[i] - l[i - 1] >= -3 for i in range(1, len(l))):
+                ans += 1
     return ans
 
 
 def part2(filename="input.txt"):
     with Path(__file__).with_name(filename).open("r") as f:
         lines = f.read().splitlines()
+    
     ans = 0
+    def safe(l):
+        if l == list(sorted(l)):
+            if all(1 <= l[i] - l[i - 1] <= 3 for i in range(1, len(l))):
+                return 1
+        elif l == list(sorted(l, reverse=True)):
+            if all(-1 >= l[i] - l[i - 1] >= -3 for i in range(1, len(l))):
+                return 1
+    for l in lines:
+        l = list(map(int, l.split()))
+        if safe(l):
+            ans += 1
+            continue
+        for i in range(len(l)):
+            newL = l[:i] + l[i+1:]
+            if safe(newL):
+                ans += 1
+                break
     return ans
 
 if __name__ == "__main__":
@@ -31,5 +58,6 @@ if __name__ == "__main__":
     print(p1ans := part1())
     print(part2("sample.txt"))
     print(p2ans := part2())
+    
     # aocd.submit(p1ans)
     # aocd.submit(p2ans)
