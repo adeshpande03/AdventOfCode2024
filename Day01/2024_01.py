d3a22ad066be9566159672cdcd4f0dc197280185
Akhil Deshpande <deshpande.akhil.s@gmail.com>
2024-12-01 20:59:20 -0600

    day 2 init

--- Day01/2024_01.py
+++ Day01/2024_01.py
@@ -1,5 +1,8 @@
 from pathlib import Path
 from collections import Counter
+from aocd import get_data, submit
+import subprocess
+
 
 def part1(filename="input.txt"):
     with Path(__file__).with_name(filename).open("r") as f:
@@ -16,7 +19,6 @@ def part1(filename="input.txt"):
     for i in range(len(l1)):
         t += abs(l1[i] - l2[i])
     return t
-    
 
 
 def part2(filename="input.txt"):
@@ -34,8 +36,25 @@ def part2(filename="input.txt"):
         t += i * l2[i]
     return t
 
+
 if __name__ == "__main__":
+    day = 1
+    year = 2024
+    
+    result = subprocess.run(
+        ["aocd", f"{year}", f"{day}", "--example"], capture_output=True, text=True
+    )
+    lines = result.stdout.splitlines()
+    trimmed_lines = lines[3:-6]
+    with Path(__file__).with_name("sample.txt").open("w") as sampleData:
+        sampleData.write("\n".join(trimmed_lines))
+    with Path(__file__).with_name("input.txt").open("w") as inputData:
+        inputData.write(get_data(day=day, year=year))
+        
     print(part1("sample.txt"))
-    print(part1())
+    print(p1ans := part1())
     print(part2("sample.txt"))
-    print(part2())
+    print(p2ans := part2())
+    
+    # submit(p1ans)
+    # submit(p2ans)
